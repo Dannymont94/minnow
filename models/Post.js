d7754f01728552417e04f056718630a11a979bf1
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-09-10 20:46:41 -0400

    created Post and Favorite models

---
+++ models/Post.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    path: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    caption: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    palette: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+  }
+);
+
+module.exports = Post;
