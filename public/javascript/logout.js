1622a9cacf3c075b716888fc49b0d4dae80e7b4b
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-09-14 09:47:58 -0400

    created front-end logout logic

---
+++ public/javascript/logout.js
@@ -0,0 +1,15 @@
+async function logout() {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (response.ok) {
+    // redirect user to home page
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.getElementById('logout').addEventListener('click', logout);
