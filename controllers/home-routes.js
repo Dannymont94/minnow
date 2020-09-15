896cfe90d7539ef3f94c45d989c275076eff2e1d
Coodie <65614373+CodyRobbins99@users.noreply.github.com>
2020-09-15 11:53:47 -0400

    dashboard and home routes added to controllers

---
+++ controllers/home-routes.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+const {User, Post} = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.status(200).render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.status(200).render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.status(200).render('signup');
+});
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+
+        res.status(200).render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn 
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes)
 
 // searched path does not exist
 router.use((req, res) => {
