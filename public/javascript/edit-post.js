056a85b46a487d35834c891d87ab12e038e936a8
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-09-15 18:14:35 -0400

    created logic for editing a post's caption and deleting a post

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,27 @@
+const captionInput = document.getElementById('caption-input');
+const postEl = document.getElementById('post');
+
+async function editFormHandler(event) {
+  event.preventDefault();
+
+  const caption = captionInput.value.trim() || null;
+  const id = postEl.dataset.postId;
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      caption
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.getElementById('edit-post-form').addEventListener('submit', editFormHandler);
