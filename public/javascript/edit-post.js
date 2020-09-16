1fb1361f1a6772d96cc7387e7c8db075c4ed125c
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-09-16 14:48:32 -0400

    combined delete and edit logic so that elements only needed to be named once

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -24,4 +24,21 @@ async function editFormHandler(event) {
   }
 }
 
+async function deleteFormHandler(event) {
+  event.preventDefault();
+  
+  const id = postEl.dataset.postId;
+  
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'DELETE'
+  });
+  
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+}
+
 document.getElementById('edit-post-form').addEventListener('submit', editFormHandler);
+document.getElementById('delete-post-btn').addEventListener('click', deleteFormHandler);
